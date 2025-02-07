4b22295650867cdd3b3f3462ceeac3c9be70f59c
Joselyn-Alejandra-Valenzuela-0552 <164534000+Joselyn-Alejandra-Valenzuela-0412@users.noreply.github.com>
2025-02-07 16:47:44 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,18 @@
+
+// funcion que no devuelve valor
+//FUNCION SALUDAR
+void saludar(){
+  print("¡Hola, mundo!");
+}
+void escribeNombre(){
+  print("Joselyn Alejandra Valenzuela");
+}
+void main(){
+  // JOSELYN ALEJANDRA VALENZUELA mat: 21308051280552
+  //LLAMAR FUNCION SALUDAR
+  print("Joselyn Alejandra Valenzuela mat: 21308051280552");
+  print("Llamando a la funcion saludar");
+  saludar();
+ print("Llamando a la funcion escribeNombre");
+  escribeNombre();
+}
