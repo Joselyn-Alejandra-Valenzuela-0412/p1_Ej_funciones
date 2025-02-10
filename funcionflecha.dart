36028d5092f980a0061fc5a8facd860202623e63
Joselyn-Alejandra-Valenzuela-0552 <164534000+Joselyn-Alejandra-Valenzuela-0412@users.noreply.github.com>
2025-02-10 18:32:24 +0000

    publique funcion flecha

---
+++ funcionflecha.dart
@@ -0,0 +1,29 @@
+int multiplicacion(int a, int b){
+return a* b;
+}
+
+int flechamultiplicacion(int a, int b) => a * b;
+//returnType functionName(parameters...) => expression;
+
+
+//funcion division
+double divide(double c, double d){
+  return c / d;
+}
+
+double flechadivide(double c, double d) => c / d;
+
+//funcion main
+void main(){
+  print("Joselyn Alejandra Valenzuela mat: 21308051280552");
+  print("Llamando a la funcion multiplicacion");
+  print(multiplicacion(8, 5));
+   print("Llamando a la funcion fechamultiplicacion");
+  print(flechamultiplicacion(8, 5));
+
+print("Llamando a la funcion divide");
+  print(divide(11, 5));
+   print("Llamando a la funcion flechadivide");
+  print(flechadivide(11, 5));
+
+}
--- recursos/funcionsuma.md
+++ recursos/funcionsuma.md
@@ -1,2 +1,7 @@
 Ejemploo de salida
+
 ![alt text](image-1.png)
+
+ejemplo de salida funcion normal + funcion flecha
+
+![alt text](image.png)
